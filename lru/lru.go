038c8d2467727c483011f61ed475d84d6f15586b
mac <690661404@qq.com>
2020-07-14 17:27:17 +0800

    新增了lru算法实现

--- lru/lru.go
+++ lru/lru.go
@@ -1,6 +1,86 @@
 package lru
 
+import "container/list"
+
 type Cache struct {
+	//缓存允许使用的最大内存,int64相当于long
 	maxBytes int64
-	nBytes   int64
+	//当前已使用的内存
+	nBytes int64
+	//双向链表
+	ll *list.List
+	//map映射，指向链表节点
+	cache map[string]*list.Element
+	//移除缓存
+	onEvicted func(key string, value Value)
+}
+
+type node struct {
+	key   string
+	value Value
+}
+
+type Value interface {
+	Len() int
+}
+
+//构造一个缓存实例
+func New(maxBytes int64, onEvicted func(string, Value)) *Cache {
+	return &Cache{
+		maxBytes:  maxBytes,
+		ll:        list.New(),
+		cache:     make(map[string]*list.Element),
+		onEvicted: onEvicted,
+	}
+}
+
+func (cache *Cache) Get(key string) (value Value, ok bool) {
+	if element, ok := cache.cache[key]; ok {
+		//将节点移到队首
+		cache.ll.MoveToFront(element)
+		kv := element.Value.(*node)
+		return kv.value, ok
+	}
+	return
+}
+
+func (cache *Cache) RemoveOldest() {
+	//取到队尾节点
+	tail := cache.ll.Back()
+	if tail != nil {
+		//删除节点
+		cache.ll.Remove(tail)
+		kv := tail.Value.(*node)
+		//删除map映射中的缓存
+		delete(cache.cache, kv.key)
+		//计算剩余内存
+		cache.nBytes -= int64(len(kv.key)) + int64(kv.value.Len())
+		//若回调函数非空，则执行回调函数
+		if cache.onEvicted != nil {
+			cache.onEvicted(kv.key, kv.value)
+		}
+	}
+}
+
+func (cache *Cache) Set(key string, value Value) {
+	if element, ok := cache.cache[key]; ok {
+		cache.ll.MoveToFront(element)
+		kv := element.Value.(*node)
+		//加上新value内存，减去原value内存
+		cache.nBytes += (int64(value.Len()) - int64(kv.value.Len()))
+		kv.value = value
+	} else {
+		//若缓存中还没有该节点，插入一个
+		element := cache.ll.PushFront(&node{key: key, value: value})
+		//添加映射
+		cache.cache[key] = element
+		cache.nBytes += int64(len(key)) + int64(value.Len())
+	}
+	for cache.maxBytes != 0 && cache.nBytes > cache.maxBytes {
+		cache.RemoveOldest()
+	}
+}
+
+func (cache *Cache) Len() int {
+	return cache.ll.Len()
 }
---
+++ lru/lru_test.go
@@ -0,0 +1,21 @@
+package lru
+
+import "testing"
+
+type String string
+
+func (s String) Len() int {
+	return len(s)
+}
+
+func Test(t *testing.T) {
+	lru := New(int64(0), nil)
+	lru.Set("key1", String("1234"))
+
+	if v, ok := lru.Get("key1"); !ok || string(v.(String)) != "1234" {
+		t.Fatalf("cache hit key1=1234 failed")
+	}
+	if _, ok := lru.Get("key2"); ok {
+		t.Fatalf("cache miss key2 failed")
+	}
+}
