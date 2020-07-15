c2bf45bd08390d25a6b5f0e5df32efdd019fc998
mac <690661404@qq.com>
2020-07-15 20:31:36 +0800

    新增并发测试

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
+var m sync.Mutex
+
+var set = make(map[int]bool)
+
+func printOnce(num int) {
+	m.Lock()
+	defer m.Unlock()
+	if _, exist := set[num]; !exist {
+		fmt.Println(num)
+	}
+	set[num] = true
+}
+func main() {
+	for i := 0; i < 10; i++ {
+		go printOnce(100)
+	}
+	time.Sleep(time.Second)
 }
