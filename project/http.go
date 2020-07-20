caee35bf2a8b4e56795cef558dd0ae5bb7927459
mac <690661404@qq.com>
2020-07-20 17:09:00 +0800

    新增了分布式节点的配合处理。

--- project/http.go
+++ project/http.go
@@ -2,18 +2,35 @@ package project
 
 import (
 	"fmt"
+	"github.com/Threadalive/gocache/project/consistent_hash"
 	"github.com/Threadalive/gwf"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"strings"
+	"sync"
 )
 
-const defaultBasePath = "/_gocache/"
+const (
+	defaultBasePath = "/_gocache/"
+	defaultReplicas = 50
+)
 
 //承载http通信的结构
 type HttpPool struct {
 	self     string
 	basePath string
+	mu       sync.Mutex
+	//服务器节点的哈希环
+	peers *consistent_hash.Map
+	//记录远程节点与其对应的httpGetter
+	httpGetter map[string]*HttpGetter
+}
+
+//http客户端类
+type HttpGetter struct {
+	baseURL string
 }
 
 //新建一个HttpPool
@@ -26,6 +43,32 @@ func NewHttpPool(self string) *HttpPool {
 
 func (p *HttpPool) Log(format string, v ...interface{}) {
 	log.Printf("[Server %s] %s", p.self, fmt.Sprintf(format, v...))
+}
+
+//注册服务器节点
+func (p *HttpPool) Set(peers ...string) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	//根据默认的副本数和默认哈希算法新建哈希结构
+	p.peers = consistent_hash.New(defaultReplicas, nil)
+	//添加服务器节点
+	p.peers.AddNodes(peers...)
+	p.httpGetter = make(map[string]*HttpGetter, len(peers))
+	for _, peer := range peers {
+		p.httpGetter[peer] = &HttpGetter{baseURL: peer + p.basePath}
+	}
+}
+
+//根据key选取所需节点
+func (p *HttpPool) PickPeer(key string) (PeerGetter, bool) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	if peer := p.peers.Get(key); peer != "" && peer != p.self {
+		p.Log("Pick peer %s", peer)
+		return p.httpGetter[peer], true
+	}
+	return nil, false
 }
 
 //添加节点的服务端操作
@@ -59,4 +102,26 @@ func (p *HttpPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 	context.SetHeader("Content-Type", "application/octet-stream")
 	context.String(http.StatusOK, string(view.ByteSlice()))
+}
+
+func (h *HttpGetter) Get(group string, key string) ([]byte, error) {
+	acceptUrl := fmt.Sprintf("%v%v/%v", h.baseURL, url.QueryEscape(group), url.QueryEscape(key))
+
+	resp, err := http.Get(acceptUrl)
+
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("server returned: %v", resp.Status)
+	}
+
+	bytes, err2 := ioutil.ReadAll(resp.Body)
+
+	if err2 != nil {
+		return nil, fmt.Errorf("reading response body: %v", err)
+	}
+	return bytes, nil
 }
